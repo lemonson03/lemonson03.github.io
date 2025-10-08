f92d89fb7150462553f5ad33142f693aa600a20f
qorjiwon <wldnjsl2001@gmail.com>
2025-10-08 21:56:24 +0900

    change config options

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',           // 정적 사이트로 내보내기
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
